8d630391dcc117f91806d379ab99beec146e32dd
magaliboulanger <56554227+magaliboulanger@users.noreply.github.com>
2024-02-07 16:42:10 -0300

    Code used for lesson 23

--- cypress/e2e/fistTest.spec.js
+++ cypress/e2e/fistTest.spec.js
@@ -74,7 +74,7 @@ describe('First test suite', () =>{
             .click()
     })
 
-    it.only('subject of the command',()=>{
+    it('subject of the command',()=>{
         //visit base url
         cy.visit('/')
         //navigate through the tabs by text 
@@ -103,6 +103,42 @@ describe('First test suite', () =>{
             cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
         })
 
+    })
+
+    it.only('extract text values', ()=>{
+        //visit base url
+        cy.visit('/')
+        //navigate through the tabs by text 
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain','Email address')
+
+        //2
+        cy.get('[for=exampleInputEmail1]').then(label=>{
+            const labelText = label.text()
+            expect(labelText).to.equal('Email address')
+            cy.wrap(labelText).should('contain','Email address')
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+        cy.get('[for=exampleInputEmail1]').invoke('text').as('labelText').should('contain','Email address')
+
+        //4
+        cy.get('[for=exampleInputEmail1]').invoke('attr', 'class').then(classValue=>{
+            expect(classValue).to.equal('label')
+        })
+
+        //5 invoke properties
+        cy.get('[for=exampleInputEmail1]').type('test@test.com')
+        cy.get('[for=exampleInputEmail1]').invoke('prop', 'value').should('contain','test@test.com').then(property => {
+            expect(property).to.equal("test@test.com")
+        })
     })
     
 })
