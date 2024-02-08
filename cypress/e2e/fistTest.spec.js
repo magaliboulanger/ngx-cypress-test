77ad8b6f4edd3fe998b6a4bac043d3c8ff5b7907
magaliboulanger <56554227+magaliboulanger@users.noreply.github.com>
2024-02-08 14:36:46 -0300

    Section 4 completed.

--- cypress/e2e/fistTest.spec.js
+++ cypress/e2e/fistTest.spec.js
@@ -157,7 +157,7 @@ describe('First test suite', () =>{
         )
     })
 
-    it.only('checkbox', ()=>{
+    it('checkbox', ()=>{
         //visit base url
         cy.visit('/')
         //navigate through the tabs by text 
@@ -174,4 +174,150 @@ describe('First test suite', () =>{
         cy.get('[type="checkbox"]').eq(0).click({force: true})
         cy.get('[type="checkbox"]').eq(1).click({force: true})
     })
+
+    it('Date picker', ()=> {
+        
+        function selectDayFromCurrent(day){
+            //this is a recursive function because we need to repeat this as it is necessary and find the desered date, and javascript is asynchronous
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleDateString('en-US', {month:'short'})
+            let futureYear = date.getFullYear()
+            let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+                if(!dateAttribute.includes(futureMonth) || !dateAttribute.includes(futureYear)){
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('.day-cell').not('.bounding-month').contains(futureDay).click()
+                }
+            })
+            return dateToAssert
+        }
+
+            //visit base url
+        cy.visit('/')
+        //navigate through the tabs by text 
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+            cy.wrap(input).click()
+            const dateToAssert = selectDayFromCurrent(200)
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateToAssert)
+            cy.wrap(input).should('have.value', dateToAssert)
+        
+        })
+
+    })
+
+    it('Lists and dropdowns', ()=> {
+        //visit base url
+        cy.visit('/')
+
+        //1
+        cy.get('nav').find('nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav').find('nb-select').should('contain', 'Dark')
+
+        //2
+        cy.get('nav').find('nb-select').then(dropDown=>{
+            cy.wrap(dropDown).click()
+            cy.get('.options-list nb-option').each((listItem, index) =>{
+                const itemText = listItem.text().trim()
+                cy.wrap(listItem).click()
+                cy.wrap(dropDown).should('contain', itemText)
+                if (index<3){
+                    cy.wrap(dropDown).click() 
+                }
+            })
+        })
+    })
+
+
+
+
+
+    it('Tables', ()=> {
+        //visit base url
+        cy.visit('/')
+        //navigate through the tabs by text 
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1 - Get the row by text
+        cy.get('tbody').contains('tr', 'Larry').then(tableRow=>{
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type("25")
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        //2 - Get row by index
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then( tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('John')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Smith')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+        })
+        cy.get('tbody tr').first().find('td').then(tableColumns=>{
+            cy.wrap(tableColumns).eq(2).should('contain', 'John')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Smith')
+        })
+
+        //3 Get each row validation
+        const age = [20, 30, 40, 200]
+        cy.wrap(age).each(age => {
+            cy.get('thead [placeholder="Age"]').clear().type(age)
+            //wait half a second - page has a little animation 
+            cy.wait(500)
+            cy.get('tbody tr').each(tableRow=>{
+                if (age == 200){
+                    cy.wrap(tableRow).should('contain', 'No data found')
+                } else {
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                }
+            })
+        })
+    })
+
+    it('tooltips', ()=> {
+        //visit base url
+        cy.visit('/')
+        //navigate through the tabs by text 
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+
+        cy.contains('nb-card', 'Colored Tooltips').contains('Default').click()
+        cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+
+    })
+
+    it.only('dialog box', ()=> {
+        //visit base url
+        cy.visit('/')
+        //navigate through the tabs by text 
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1 This is not a good approach because if the alert is not present, the assertion wont fail, because it wnot be executed
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', (confirm)=>{
+            expect(confirm).to.equal('Are you sure you want to delete?')
+        })
+
+        //2
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(()=>{
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        })
+
+        //3
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', ()=>false)
+        
+    })
+
 })
